8baa64af3b1d160695e648cde29f4ebc8d196f8d
The Natural <philip.smalls86@gmail.com>
2019-06-19 13:44:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +11,20 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
+def help
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choosr a song to play
+        - exit : exits this program"
+end
+
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  songs.each do |key, val|
+   binding.pry
+  end 
+end 
 
